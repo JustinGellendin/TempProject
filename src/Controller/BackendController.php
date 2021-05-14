d0bbfd68f464d44ff0ae81f3b69fa68fb4cb5a98
JustinGellendin <justin.gellendin@outlook.de>
2021-05-14 21:00:28 +0200

    is git a bit stupid today?

--- src/Controller/BackendController.php
+++ src/Controller/BackendController.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 namespace App\Controller;
@@ -17,14 +16,17 @@ class BackendController extends AbstractController
     {
         $lastLogin = null;
         $user = $this->getUser();
-        if ($user && $user instanceof User){
+
+        if ($user)
+        {
             $lastLogin = $user->getLastLogin();
         }
 
         return $this->render('backend/start.html.twig',
-        [
-            'lastLogin' => $lastLogin
-        ]);
+            [
+                'lastLogin' => $lastLogin
+            ]
+        );
     }
 
     /**
@@ -42,49 +44,4 @@ class BackendController extends AbstractController
     {
         return $this->render('backend/temps.html.twig');
     }
-=======
-<?php
-
-namespace App\Controller;
-
-use App\Entity\User;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Response;
-
-class BackendController extends AbstractController
-{
-    /**
-     * @Route("/backend", name="backend")
-     */
-    public function backend():Response
-    {
-        $lastLogin = null;
-        $user = $this->getUser();
-        if ($user && $user instanceof User){
-            $lastLogin = $user->getLastLogin();
-        }
-
-        return $this->render('backend/start.html.twig',
-        [
-            'lastLogin' => $lastLogin
-        ]);
-    }
-
-    /**
-     * @Route("/backend/users", name="users")
-     */
-    public function users():Response
-    {
-        return $this->render('backend/user.html.twig');
-    }
-
-    /**
-     * @Route("/backend/temperatures", name="temperatures")
-     */
-    public function temperatures():Response
-    {
-        return $this->render('backend/temps.html.twig');
-    }
->>>>>>> adds eventually some changes i don't remember doing
 }
