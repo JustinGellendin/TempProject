3c42e8a45d6399fdb4e5fbdc3b1924c0a981ef80
JustinGellendin <justin.gellendin@outlook.de>
2021-05-14 20:47:25 +0200

    adds eventually some changes i don't remember doing

--- src/Controller/BackendController.php
+++ src/Controller/BackendController.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 
 namespace App\Controller;
@@ -41,4 +42,49 @@ class BackendController extends AbstractController
     {
         return $this->render('backend/temps.html.twig');
     }
+=======
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class BackendController extends AbstractController
+{
+    /**
+     * @Route("/backend", name="backend")
+     */
+    public function backend():Response
+    {
+        $lastLogin = null;
+        $user = $this->getUser();
+        if ($user && $user instanceof User){
+            $lastLogin = $user->getLastLogin();
+        }
+
+        return $this->render('backend/start.html.twig',
+        [
+            'lastLogin' => $lastLogin
+        ]);
+    }
+
+    /**
+     * @Route("/backend/users", name="users")
+     */
+    public function users():Response
+    {
+        return $this->render('backend/user.html.twig');
+    }
+
+    /**
+     * @Route("/backend/temperatures", name="temperatures")
+     */
+    public function temperatures():Response
+    {
+        return $this->render('backend/temps.html.twig');
+    }
+>>>>>>> adds eventually some changes i don't remember doing
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 
 namespace App\Entity;
@@ -143,3 +144,150 @@ class User implements UserInterface
         // $this->plainPassword = null;
     }
 }
+=======
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @ORM\Table(name="`user`")
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     * @var \DateTime
+     */
+    private $lastLogin;
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->username;
+    }
+
+    /**
+     * @param string $username
+     *
+     * @return $this
+     */
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
+    }
+
+    /**
+     * @param string $password
+     *
+     * @return $this
+     */
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getLastLogin(): ?\DateTime
+    {
+        return $this->lastLogin;
+    }
+
+    /**
+     * @param \DateTime $lastLogin
+     */
+    public function setLastLogin(\DateTime $lastLogin): void
+    {
+        $this->lastLogin = $lastLogin;
+    }
+
+    /**
+     * Returning a salt is only needed, if you are not using a modern
+     * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
+     *
+     * @see UserInterface
+     */
+    public function getSalt(): ?string
+    {
+        return null;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+}
+>>>>>>> adds eventually some changes i don't remember doing
