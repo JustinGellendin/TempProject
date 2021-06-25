6b5d6d5808a776e232d6727c1d9bf280094e6c0b
Frechdaxx935 <83698454+Frechdaxx935@users.noreply.github.com>
2021-06-25 11:03:32 +0200

    datenbankabfrage probiert

---
+++ TempAdmin/src/temperaturüberwachung/TemperaturÜberwachung.java
@@ -0,0 +1,62 @@
+package temperaturüberwachung;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+/**
+ *
+ * @author samyo
+ */
+public class TemperaturÜberwachung {
+
+    public static void main(String[] args) {
+        
+        String url = "jdbc:mysql://85.214.105.212:3306/school?serverTimezone=Europe/Paris";
+        String user = "Samuel";
+        String pass = "vK?327ec";
+        Connection conn = null;
+        
+        try {
+        // Verbindung aufbauen
+        conn = DriverManager.getConnection(url, user, pass);
+        System.out.println("Verbindung erfolgreich hergestellt");
+        } 
+        catch (SQLException e) {
+            System.out.println("Fehler:");
+            System.out.println(e.getMessage());
+        }
+        
+        System.out.println("Willkommen in der Temperaturüberachung! Bitte Anmelden");
+        System.out.println("Nutzername:");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println("Passwort:");
+        String passw = sc.next();
+        
+        try
+        {
+        Statement stm = conn.createStatement();
+        ResultSet rs = stm.executeQuery("SELECT * FROM user");
+        }
+        catch (Exception e)
+        {
+            System.out.println("Fehler:");
+            System.out.println(e.getMessage());
+        }
+         System.out.println("");
+        
+        try
+        {
+        conn.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println("Fehler:");
+            System.out.println(e.getMessage());
+        }
+    }
+}
