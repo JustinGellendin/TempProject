fbd6a8b91d35bbbc8a08b89ff963dfa2b3365a42
derbilbo <jakobkaiser@hotmail.de>
2021-06-29 11:23:25 +0200

    Addes tings

--- TempAdmin/src/temperaturüberwachung/TemperaturÜberwachung.java
+++ TempAdmin/src/temperaturüberwachung/TemperaturÜberwachung.java
@@ -79,6 +79,7 @@ public class TemperaturÜberwachung {
             }
         }
         Sensory.modifyAdress(con);
+        Sensory.modifyRack(con);
         //Datenbankverbindung schließen
         try{
         con.close();
